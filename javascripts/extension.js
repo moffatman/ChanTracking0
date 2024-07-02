6b08b2f6f3346ac78aa18e2ef0697096c5e9503f
BOT <actions@users.noreply.github.com>
2024-07-02 01:13:51 +0000

    M  javascripts/extension.js

--- javascripts/extension.js
+++ javascripts/extension.js
@@ -6122,9 +6122,8 @@ ThreadUpdater.init = function() {
   this.hidden = 'hidden';
   this.visibilitychange = 'visibilitychange';
   
-  this.adRefreshDelay = 1000;
-  this.adDebounce = 0;
-  this.ads = {};
+  this.adsTTL = 900000;
+  this.adsReloadTs = 0;
   
   if (typeof document.hidden === 'undefined') {
     if ('mozHidden' in document) {
@@ -6141,7 +6140,6 @@ ThreadUpdater.init = function() {
     }
   }
   
-  this.initAds();
   this.initControls();
   
   document.addEventListener('scroll', this.onScroll, false);
@@ -6344,7 +6342,6 @@ ThreadUpdater.onVisibilityChange = function() {
   }
   else {
     self.delayId = 0;
-    self.refreshAds();
   }
   
   self.timeLeft = self.delayRange[0];
@@ -6359,8 +6356,6 @@ ThreadUpdater.onScroll = function() {
       && !document[ThreadUpdater.hidden])) {
     ThreadUpdater.clearUnread();
   }
-  
-  ThreadUpdater.refreshAds();
 };
 
 ThreadUpdater.clearUnread = function() {
@@ -6464,110 +6459,29 @@ ThreadUpdater.checkTailValid = function(posts) {
   return !!$.id('p' + op.tail_id);
 };
 
-ThreadUpdater.initAds = function() {
-  var i, id, adIds = [ '_top_ad', '_middle_ad', '_bottom_ad' ];
-  
-  for (i = 0; id = adIds[i]; ++i) {
-    ThreadUpdater.ads[id] = {
-      time: 0,
-      seenOnce: false,
-      isStale: false
-    };
-  }
-};
-
-ThreadUpdater.invalidateAds = function() {
-  var id, meta;
-  
-  for (id in ThreadUpdater.ads) {
-    meta = ThreadUpdater.ads[id];
-    if (meta.seenOnce) {
-      meta.isStale = true;
-    }
-  }
-};
-
-ThreadUpdater.refreshAds = function() {
-  var self, now, el, id, ad, meta, hidden, docHeight, offset;
-  
-  self = ThreadUpdater;
-  
-  now = Date.now();
-  
-  if (now - self.adDebounce < 100) {
+ThreadUpdater.reloadAds = function() {
+  if (!window.Danbo || !window.reloadAdsDanbo) {
     return;
   }
   
-  self.adDebounce = now;
+  let self = ThreadUpdater;
   
-  hidden = document[self.hidden];
-  docHeight = document.documentElement.clientHeight;
+  let now = Date.now();
   
-  for (id in self.ads) {
-    meta = self.ads[id];
-    
-    if (hidden) {
-      continue;
-    }
-    
-    ad = window[id];
-    
-    if (!ad) {
-      continue;
-    }
-    
-    el = $.id(ad.D);
-    
-    if (!el) {
-      continue;
-    }
-    
-    offset = el.getBoundingClientRect();
-    
-    if (offset.top < 0 || offset.bottom > docHeight) {
-      continue;
-    }
-    
-    meta.seenOnce = true;
-    
-    if (!meta.isStale || now - meta.time < self.adRefreshDelay) {
-      continue;
-    }
-    
-    meta.time = now;
-    meta.isStale = false;
-    
-    ados_refresh(ad, 0, false);
+  if (!self.adsReloadTs) {
+    self.adsReloadTs = now;
+    return;
   }
+  
+  if (now - self.adsReloadTs < self.adsTTL) {
+    return;
+  }
+  
+  self.adsReloadTs = now;
+  
+  window.reloadAdsDanbo();
 };
-/*
-ThreadUpdater.markDeletedReplies = function(newposts) {
-  var i, j, posthash, oldposts, el;
-  
-  posthash = {};
-  for (i = 0; j = newposts[i]; ++i) {
-    posthash['pc' + j.no] = 1;
-  }
-  
-  oldposts = $.cls('replyContainer');
-  for (i = 0; j = oldposts[i]; ++i) {
-    if (!posthash[j.id] && !$.hasClass(j, 'deleted')) {
-      if (this.deletionQueue[j.id]) {
-        el = document.createElement('img');
-        el.src = Main.icons2.trash;
-        el.className = 'trashIcon';
-        el.title = 'This post has been deleted';
-        $.addClass(j, 'deleted');
-        $.cls('postNum', j)[1].appendChild(el);
-        delete this.deletionQueue[j.id];
-      }
-      else {
-        this.deletionQueue[j.id] = 1;
-      }
-    }
-  }
-};
-*/
+
 ThreadUpdater.onload = function() {
   var i, state, self, nodes, thread, newposts, frag, lastrep, lastid,
     op, doc, autoscroll, count, fromQR, lastRepPos;
@@ -6649,11 +6563,7 @@ ThreadUpdater.onload = function() {
       fromQR = true;
       QR.lastReplyId = null;
     }
-    /*
-    if (!fromQR) {
-      self.markDeletedReplies(newposts);
-    }
-    */
+    
     if (count) {
       doc = document.documentElement;
       
@@ -6723,8 +6633,9 @@ ThreadUpdater.onload = function() {
         ThreadStats.update(op.replies, op.images, op.unique_ips, op.bumplimit, op.imagelimit);
       }
       
-      self.invalidateAds();
-      self.refreshAds();
+      if (self.force) {
+        self.reloadAds();
+      }
       
       UA.dispatchEvent('4chanThreadUpdated', { count: count });
     }
